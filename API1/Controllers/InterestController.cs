6907e8ea6e1e4ec275769732894e20feac5f112a
Vitor Lima <vitorjrfoca@gmail.com>
2021-03-15 18:22:37 -0400

    Add endpoint "taxaJuros"

---
+++ API1/Controllers/InterestController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InterestController : ControllerBase
+    {
+        [HttpGet("taxaJuros")]
+        public IActionResult GetInterestRate()
+            => Ok(0.01m);
+    }
+}
