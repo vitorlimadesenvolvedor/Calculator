f96fd19f8047485577426eaf42d465c4f6d0b79a
Vitor Lima <vitorjrfoca@gmail.com>
2021-03-15 18:23:27 -0400

    Add Swagger config

--- API1/Startup.cs
+++ API1/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace API1
 {
@@ -25,6 +26,11 @@ namespace API1
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API 1", Version = "v1", Description = "API about interest" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +48,14 @@ namespace API1
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(opt =>
+            {
+                opt.SwaggerEndpoint("/swagger/v1/swagger.json", "API");
+                opt.RoutePrefix = string.Empty;
             });
         }
     }
