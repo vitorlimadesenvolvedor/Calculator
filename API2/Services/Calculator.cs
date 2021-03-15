732fb5f7b3b42d8335e2b7f21db1756bc786a151
Vitor Lima <vitorjrfoca@gmail.com>
2021-03-15 18:03:33 -0400

    Add method CalculateInterest

---
+++ API2/Services/Calculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace API2.Services
+{
+    public class Calculator
+    {
+        public decimal CalculateInterest(decimal initialValue, decimal interestRate, int months)
+        {
+            if (months < 0)
+                months = Math.Abs(months);
+
+            if (initialValue < 0)
+                initialValue = Math.Abs(initialValue);
+
+            decimal result = initialValue * (decimal)(Math.Pow((double)(1 + interestRate), months));
+            decimal roundedResult = Math.Round(result, 2);
+
+            return roundedResult;
+        }
+    }
+}
