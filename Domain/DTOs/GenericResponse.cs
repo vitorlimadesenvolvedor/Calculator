9c1f0dd0f46ebb8092455628859aecf2100eba49
Vitor Lima <vitorjrfoca@gmail.com>
2021-03-18 12:54:19 -0400

    Add Helper ApiRequest

---
+++ Domain/DTOs/GenericResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.DTOs
+{
+
+    public class GenericResponse
+    {
+        public GenericResponse(bool success, string message, object data = null)
+        {
+            Success = success;
+            Message = message;
+            Data = data;
+        }
+
+        public bool Success { get; }
+        public string Message { get; }
+        public object Data { get; }
+
+    }
+}
---
+++ Domain/Helpers/ApiRequest.cs
@@ -0,0 +1,47 @@
+﻿using Domain.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Helpers
+{
+    public class ApiRequest
+    {
+        private readonly HttpClient _client;
+        public ApiRequest(string url)
+        {
+            _client = new HttpClient
+            {
+                BaseAddress = new Uri(url)
+            };
+        }
+
+        public async Task<GenericResponse> Get(string url)
+        {
+            try
+            {
+                var response = await _client.GetAsync(url);
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    var data = await response.Content.ReadAsStringAsync();
+                    var result = new GenericResponse(true, "", data);
+                    return result;
+                }
+                else
+                {
+                    return new GenericResponse(false, response.ReasonPhrase);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return new GenericResponse(false, $"Url: {_client.BaseAddress}{url} :{ex.Message}");
+            }
+
+        }
+    }
+}
