7cd7850974047d61015c32307d8bc6f60b0347fc
Vitor Lima <vitorjrfoca@gmail.com>
2021-03-18 12:57:40 -0400

    Add CalculatorService

---
+++ Domain/Services/CalculatorService.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Helpers;
+using Domain.Services.Interfaces;
+using System.Threading.Tasks;
+
+namespace Domain.Services
+{
+    public class CalculatorService : ICalculatorService
+    {
+        private readonly IInterestRateService _interestRateService;
+
+        public CalculatorService(IInterestRateService interestRateService)
+        {
+            _interestRateService = interestRateService;
+        }
+
+        public async Task<decimal> GetTotalCompoundInterestGain(decimal initialValue, int months)
+        {
+            decimal interestRate = await _interestRateService.Get();
+
+            return Calculator.CalculateCompoundInterest(initialValue, interestRate, months);
+        }
+    }
+}
---
+++ Domain/Services/Interfaces/ICalculatorService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Services.Interfaces
+{
+    public interface ICalculatorService
+    {
+        Task<decimal> GetTotalCompoundInterestGain(decimal initialValue, int months);
+    }
+}
