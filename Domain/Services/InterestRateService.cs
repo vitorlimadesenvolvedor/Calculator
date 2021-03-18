d0a72ca56735d517ffa1f3bb195118592cf1c679
Vitor Lima <vitorjrfoca@gmail.com>
2021-03-18 12:53:22 -0400

    Add InterestRateService

---
+++ Domain/Services/InterestRateService.cs
@@ -0,0 +1,36 @@
+﻿using Domain.Helpers;
+using Domain.Services.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Services
+{
+    public class InterestRateService : IInterestRateService
+    {
+        private readonly ApiRequest _api = null;
+        public InterestRateService(IConfiguration configuration)
+        {
+           string url = configuration.GetSection("Services:API1").Value;
+            _api = new ApiRequest(url);
+        }
+            
+        public async Task<decimal> Get()
+        {
+           
+            var result = await _api.Get("api/interest/taxaJuros");
+
+            if (result.Success)
+            {
+               return JsonConvert.DeserializeObject<decimal>(result.Data.ToString());
+            }
+            else
+            {
+                throw new Exception($"Não foi possível obter o valor da taxa de juros: {result.Message}");
+            }
+        }
+    }
+}
