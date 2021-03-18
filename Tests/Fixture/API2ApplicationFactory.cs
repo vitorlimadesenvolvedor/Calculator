473ca45513f6f7114d6e047a13951b5b04675de5
Vitor Lima <vitorjrfoca@gmail.com>
2021-03-18 12:59:24 -0400

    Add API2 Application Factory and InterestRateStub for integration tests

---
+++ Tests/Fixture/API2ApplicationFactory.cs
@@ -0,0 +1,26 @@
+﻿using API2;
+using Domain.Services.Interfaces;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Tests.Mocks;
+
+namespace Tests.Fixture
+{
+
+    public class API2ApplicationFactory : WebApplicationFactory<Startup>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureTestServices(services =>
+            {
+                services.AddTransient<IInterestRateService, InterestRateStub>();
+            });
+        }
+    }
+
+}
---
+++ Tests/Mocks/InterestRateStub.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Mocks
+{
+    public class InterestRateStub : IInterestRateService
+    {
+        public async Task<decimal> Get()
+        {
+            return await Task.Run(() => 0.01m );
+        }
+    }
+}
