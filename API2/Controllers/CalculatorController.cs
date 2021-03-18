45b5ed28fe966082d3ff458b7778c684498b399b
Vitor Lima <vitorjrfoca@gmail.com>
2021-03-18 12:56:41 -0400

    Add endpoint "calculajuros"

---
+++ API2/Controllers/CalculatorController.cs
@@ -0,0 +1,26 @@
+﻿using Domain.DTOs;
+using Domain.Helpers;
+using Domain.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace API2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        private readonly ICalculatorService _calculatorService;
+        public CalculatorController(ICalculatorService calculatorService)
+        {
+            _calculatorService = calculatorService;
+        }
+
+        [HttpGet("calculajuros")]
+        public async Task<IActionResult> CalculateInterest([FromQuery] CalculatorDTO calculatorDTO)
+        {
+            decimal result = await _calculatorService.GetTotalCompoundInterestGain(calculatorDTO.ValorInicial, calculatorDTO.Meses);
+            return Ok(result);
+        }
+    }
+}
---
+++ Domain/DTOs/CalculatorDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Domain.DTOs
+{
+    public class CalculatorDTO : IValidatableObject
+    {
+        [Range(0, int.MaxValue, ErrorMessage = "Valor inicial deve ser um decimal positivo")]
+        public decimal ValorInicial { get; set; }
+        public int Meses { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Meses < 0)
+                yield return new ValidationResult("Número de meses deve ser um inteiro positivo", new[] { nameof(Meses) });
+
+            if (ValorInicial < 0)
+                yield return new ValidationResult("Valor inicial deve ser um decimal positivo", new[] { nameof(ValorInicial) });
+        }
+    }
+}
